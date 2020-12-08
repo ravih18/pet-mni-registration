e500b4294a86b6b9e995726f4b5445038aa2496b
ravih18 <rhassana96@gmail.com>
2020-12-08 15:58:32 +0100

    code improvement

--- samples/ANTs_test.sh
+++ samples/ANTs_test.sh
@@ -11,16 +11,18 @@
 
 module load ANTs
 
-A="/network/lustre/dtlake01/aramis/users/ravi.hassanaly/datasets/mini_adni_bids/sub-ADNI029S1384/ses-M00/pet/sub-ADNI029S1384_ses-M00_task-rest_acq-fdg_pet.nii.gz"
-B="/network/lustre/dtlake01/aramis/users/ravi.hassanaly/datasets/mini_adni_bids/sub-ADNI029S1384/ses-M00/anat/sub-ADNI029S1384_ses-M00_T1w.nii.gz"
+BIDS=/network/lustre/dtlake01/aramis/users/ravi.hassanaly/datasets/mini_adni_bids/
+A=${BIDS}sub-ADNI029S1384/ses-M00/pet/sub-ADNI029S1384_ses-M00_task-rest_acq-fdg_pet.nii.gz
+B=${BIDS}sub-ADNI029S1384/ses-M00/anat/sub-ADNI029S1384_ses-M00_T1w.nii.gz
+OUTPUT=/network/lustre/dtlake01/aramis/users/ravi.hassanaly/datasets/pet-linear/
 
 echo fixed image MRI $B
 echo moving image PET $A
 
-antsRegistrationSyNQuick.sh -d 3 -f $B -m $A -o RegA2B 
+antsRegistrationSyNQuick.sh -d 3 -f $B -m $A -o ${OUTPUT}RegPET2MRI 
 echo "RegA2B registration done"
 
-antsApplyTransforms -d 3 -i $A -o ADeformed.nii.gz -r $B -t RegA2B1Warp.nii.gz -t RegA2B0GenericAffine.mat
+antsApplyTransforms -d 3 -i $A -o ${OUTPUT}normal/PETDeformed.nii.gz -r $B -t ${OUTPUT}normal/RegPET2MRI1Warp.nii.gz -t ${OUTPUT}RegPET2MRI0GenericAffine.mat
 echo "First transformation done"
-antsApplyTransforms -d 3 -i $B -o BDeformed.nii.gz -r $A -t [RegA2B0GenericAffine.mat,1] -t RegA2B1InverseWarp.nii.gz
+antsApplyTransforms -d 3 -i $B -o ${OUTPUT}inverse/MRIDeformed.nii.gz -r $A -t [${OUTPUT}inverse/RegPET2MRI0GenericAffine.mat,1] -t ${OUTPUT}RegPET2MRI1InverseWarp.nii.gz
 echo "Second transformation done"
