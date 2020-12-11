66d726f9b315fc0522e413280e210b15cd4423b3
ravih18 <rhassana96@gmail.com>
2020-12-11 15:13:11 +0100

    changed paths syntax

--- samples/MRI2MNI.sh
+++ samples/MRI2MNI.sh
@@ -13,11 +13,11 @@ module load ANTs
 dim=3
 
 DATASET=/network/lustre/dtlake01/aramis/users/ravi.hassanaly/datasets/
-PET=${DATASET}mini_adni_bids/sub-ADNI029S1384/ses-M00/pet/sub-ADNI029S1384_ses-M00_task-rest_acq-fdg_pet.nii.gz
+MNI=${DATASET}pet-linear/mni-template/mni_icbm152_t1_tal_nlin_sym_09c.nii.gz
 MRI=${DATASET}mini_adni_bids/sub-ADNI029S1384/ses-M00/anat/sub-ADNI029S1384_ses-M00_T1w.nii.gz
-OUTPUT=RegisteredPET
-TRANSFORM=${DATASET}pet-linear/PET2MRI/RegPET2MRI
+OUTPUT=${DATASET}pet-linear/MRI2MNI/RegisteredMRI.nii.gz
+TRANSFORM=${DATASET}pet-linear/MRI2MNI/RegMRI2MNI
 
-antsRegistrationSyNQuick.sh -d $dim -t 'r' -f $MRI -m $PET -o $TRANSFORM
+antsRegistrationSyNQuick.sh -d $dim -f $MNI -m $MRI -o $TRANSFORM
 
-antsApplyTransforms -d $dim -i $PET -o ${TRANSFORM}${OUTPUT}.nii.gz -r $MRI -t ${TRANSFORM}Warped.nii.gz ${TRANSFORM}0GenericAffine.mat
+antsApplyTransforms -d $dim -i $MRI -o $OUTPUT -r $MNI -t ${TRANSFORM}1Warp.nii.gz -t ${TRANSFORM}0GenericAffine.mat
